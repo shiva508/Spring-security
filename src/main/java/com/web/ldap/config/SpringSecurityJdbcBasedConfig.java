d5d1bef0d24fbeff496473bed2b64554704e6465
shiva <ram@ram-desktop>
2018-12-03 12:14:24 +0530

    security

--- src/main/java/com/web/ldap/config/SpringSecurityJdbcBasedConfig.java
+++ src/main/java/com/web/ldap/config/SpringSecurityJdbcBasedConfig.java
@@ -1,5 +1,27 @@
 package com.web.ldap.config;
 
-public class SpringSecurityJdbcBasedConfig {
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class SpringSecurityJdbcBasedConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	DataSource dataSource;
+	EntityManager entityManager;
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+@Override
+protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	auth.jdbcAuthentication().dataSource(dataSource)
+		.withDefaultSchema()
+		.usersByUsernameQuery("")
+		.groupAuthoritiesByUsername("");
+	
+}
 }
--- src/main/java/com/web/ldap/controller/LoginController.java
+++ src/main/java/com/web/ldap/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.web.ldap.controller;
 
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +20,7 @@ public String loginpage() {
 	@PostMapping("/welcome")
 	@ResponseBody
 	public String welcome() {
+		SecurityContextHolder.getContext();
 		return "HI";
 	}
 }
