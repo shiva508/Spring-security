524fe1fff5e231deb8cbac3bcfd649cdc8254a68
shiva <ram@ram-desktop>
2018-12-11 18:30:47 +0530

    User Builder

--- src/main/java/com/web/ldap/config/SpringSecurityInMemoryBasedConfig.java
+++ src/main/java/com/web/ldap/config/SpringSecurityInMemoryBasedConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 @Configuration
 @EnableWebSecurity
@@ -13,6 +15,7 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 public class SpringSecurityInMemoryBasedConfig extends WebSecurityConfigurerAdapter{
 @Override
 protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	UserBuilder userBuilder=User.withDefaultPasswordEncoder();
 	auth.inMemoryAuthentication().passwordEncoder(NoOpPasswordEncoder.getInstance())
 	.withUser("user").password("user").roles("USER")
 	.and()
